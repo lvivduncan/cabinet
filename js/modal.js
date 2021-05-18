83ce64d826f08b4b51b285c7b2dadcb846dd5ffa
duncan <lvivduncan@gmail.com>
2021-05-18 17:21:04 +0300

    friend.html -- without jquery!

--- js/modal.js
+++ js/modal.js
@@ -1,41 +1,41 @@
 
-    // create modal window wrapper
-    const modalWrapper = document.createElement('div');
-    modalWrapper.setAttribute('id', 'modal-wrapper');
+// create modal window wrapper
+const modalWrapper = document.createElement('div');
+modalWrapper.setAttribute('id', 'modal-wrapper');
 
-    // approuve -- підтвердження
-    document.querySelector('.user-confirm button').addEventListener('click', function(){
+// approuve -- підтвердження
+document.querySelector('.user-confirm button').addEventListener('click', function(){
 
-        // add modal
-        document.body.className = 'modal-hidden';
-        document.body.append(modalWrapper);
+    // add modal
+    document.body.className = 'modal-hidden';
+    document.body.append(modalWrapper);
 
-        // show mobile basket
-        document.getElementById('approuve').classList.add('active');
-    });
+    // show mobile basket
+    document.getElementById('approuve').classList.add('active');
+});
 
-    // click close
-    modalWrapper.addEventListener('click', () => {
+// click close
+modalWrapper.addEventListener('click', () => {
 
+    // clear
+    document.body.className = '';
+    modalWrapper.remove();        
+    document.querySelectorAll('.approuve').forEach(item => item.classList.remove('active'));
+
+});
+
+// esc button
+document.addEventListener('keydown', e => {
+    if(e.code == 'Escape' || e.key == 'Escape'){
+        
         // clear
         document.body.className = '';
-        modalWrapper.remove();        
+        modalWrapper.remove();
         document.querySelectorAll('.approuve').forEach(item => item.classList.remove('active'));
+    }
+});
 
-    });
-
-    // esc button
-    document.addEventListener('keydown', e => {
-        if(e.code == 'Escape' || e.key == 'Escape'){
-            
-            // clear
-            document.body.className = '';
-            modalWrapper.remove();
-            document.querySelectorAll('.approuve').forEach(item => item.classList.remove('active'));
-        }
-    });
-
-    // робимо активною кнопку підтвердження
-    document.querySelector('#approuve input[type="password"]').addEventListener('input', function(){
-        document.querySelector('#approuve button').className = 'active';
-    });
+// робимо активною кнопку підтвердження
+document.querySelector('#approuve input[type="password"]').addEventListener('input', function(){
+    document.querySelector('#approuve button').className = 'active';
+});
